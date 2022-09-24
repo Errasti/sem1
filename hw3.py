911c3bb9703a8afc2ffe9dbbf2d88a94dfb7d93b
errasti <errasti007@yandex.ru>
2022-09-24 23:48:09 +0300

    third commit ( 1 task left )

---
+++ hw3.py
@@ -0,0 +1,12 @@
+x = int(input('Введите координату Х: '))
+y = int(input('Введите координату Y: '))
+if x > 0 and y > 0:
+    print('1')
+elif x < 0 and y > 0:
+    print('2')
+elif x < 0 and y < 0:
+    print('3')
+elif x > 0 and y < 0:
+    print('4')
+else:
+    print('Некорректный ввод координат')
---
+++ hw4.py
@@ -0,0 +1,11 @@
+quarter = int(input('Введите номер четверти: '))
+if quarter == 1:
+    print('x > 0 и y > 0')
+elif quarter == 2:
+    print('x < 0 и y > 0')
+elif quarter == 3:
+    print('x < 0 и y < 0')
+elif quarter == 4:
+    print('x > 0 и y < 0')
+else:
+    print('Некорректный ввод данных. Введите от 1 до 4')
